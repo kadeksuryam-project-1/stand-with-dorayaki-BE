77d1750faff23cbf28e4f94ffda5e84f437f205e
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2023-04-19 16:39:21 +0700

    feat: add timestamp fields

--- internal/schema/user.go
+++ internal/schema/user.go
@@ -1,6 +1,10 @@
 package schema
 
+import "time"
+
 type User struct {
-	ID    int    `json:"id" gorm:"primaryKey;column:id;type:integer"`
-	Email string `json:"email" gorm:"primaryKey;column:email;type:varchar(255)"`
+	ID        int       `json:"id" gorm:"primaryKey;column:id;type:integer"`
+	Email     string    `json:"email" gorm:"primaryKey;column:email;type:varchar(255)"`
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;not null"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at;not null"`
 }
